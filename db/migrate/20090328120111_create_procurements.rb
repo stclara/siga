fd418d1ab0318481b7989f8238cb36c74631ca6e
Marcos Augusto <x@nofxx.com>
2009-03-28 09:04:51 -0300

    procurements.... nois eh chique...

---
+++ db/migrate/20090328120111_create_procurements.rb
@@ -0,0 +1,18 @@
+class CreateProcurements < ActiveRecord::Migration
+  def self.up
+    create_table :procurements do |t|
+      t.string :name, :null => false
+      t.text :body, :null => false
+      t.timestamp :due_at
+
+      t.timestamps
+    end
+
+    add_index :procurements, :name
+    add_index :procurements, :due_at
+  end
+
+  def self.down
+    drop_table :procurements
+  end
+end
